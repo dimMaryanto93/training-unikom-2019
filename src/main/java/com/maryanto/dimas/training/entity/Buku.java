6b6c801b1b7f3d94893192df6833adb02071040f
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-01-16 12:04:08 +0700

    update mapping buku

--- src/main/java/com/maryanto/dimas/training/entity/Buku.java
+++ src/main/java/com/maryanto/dimas/training/entity/Buku.java
@@ -1,21 +1,44 @@
 package com.maryanto.dimas.training.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 @Entity
+@Table(name = "master_buku")
 public class Buku {
 
     @Id
+    @GenericGenerator(name = "uuid_gen", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid_gen")
+    @Column(name = "kode", nullable = false, unique = true, length = 64)
     private String id;
+
+    @Column(name = "isbn", nullable = false, unique = true, length = 20)
     private String isbn;
+
+    @Column(name = "title", nullable = false)
     private String judul;
+
+    @Column(name = "nama_pengarang")
     private String namaPengarang;
+
+    @Column(name = "tahun_terbit")
     private Integer tahunTerbit;
+
+    @Column(name = "nama_penerbit")
     private String namaPenerbit;
+
+    @Column(name = "created_date", nullable = false)
     private Timestamp createdDate;
+
+    @Column(name = "created_by", nullable = false)
     private String createdBy;
+
+    @Column(name = "last_update_date")
     private Timestamp lastUpdateDate;
+
+    @Column(name = "last_update_by")
     private String lastUpdateBy;
 }
